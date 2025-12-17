909d776cf8b78211a36aa180f51218d9aef412af
Alexandre <alexandre.grondin@grenoble-inp.org>
2025-12-17 02:26:50 +0100

    facto

--- PricerEngine/Capitalization.hpp
+++ PricerEngine/Capitalization.hpp
@@ -5,8 +5,7 @@
 
 using CapitalizationFunc = std::function<double(double payoff, double t_m)>;
 
-// Crée une fonction de capitalisation pour un taux r et une maturité T
-// Capitalise un flux de t_m vers T : payoff * exp(r * (T - t_m))
+// Capitalise le flux de t_m vers T 
 inline CapitalizationFunc createCapitalization(double r, double T) {
     return [r, T](double payoff, double t_m) {
         return payoff * exp(r * (T - t_m));
